5b5213d879b3ed8f2e1b87bc566db8060c35a6db
Ren-Her Hwang <hwanren@users.noreply.github.com>
2016-02-05 16:42:20 -0800

    check potential memory leak

--- hello.c
+++ hello.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct rec {
+  int i;
+  float f;
+};
+
 int main(int argc, char**argv)
 {
+  struct rec *p;
+
   printf("Hello World!\n");
-  printf("HHHHH\")
+
+  p = (struct rec *)malloc(sizeof(struct rec));
+  p->i = 1;
+  p->f = 1.0;
+  printf("a new record of (%d, %f) created\n", p->i, p->f);
+
+  /* Comment out the following free(p) to see if static analyzer picks it. */
+  /* free(p); */
 
   return 0;
 }
